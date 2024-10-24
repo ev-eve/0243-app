5979416754adeb3c5b2c08473bea48dc558bc39d
ev-eve <180556116+ev-eve@users.noreply.github.com>
2024-10-24 11:35:58 +0300

    del css, add bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,14 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>Заголовок 1</h1>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima, delectus quo et ducimus repellat deserunt, distinctio impedit tenetur sequi similique facere ipsum. Enim inventore asperiores animi, blanditiis ex mollitia soluta.
       </p>
     </>
   )
